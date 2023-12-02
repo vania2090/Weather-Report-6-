af1b572225b3dda82daec674e194699638803d20
Vania Loredo <vanialoredo.vl@gmail.com>
2023-12-02 18:16:35 -0500

    adding description

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function currentWeather(response) {
   let descriptionElement = document.querySelector("#description");
 
   cityElement.innerHTML = response.data.city;
-  descriptionElement.innerHTML = response.data.condition;
+  descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
