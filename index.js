89c168d7fed192fdd47e24f26e7148c83807dc8a
Vania Loredo <vanialoredo.vl@gmail.com>
2023-12-02 00:38:21 -0500

    changing id

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 function citySearchSubmit(event) {
   event.preventDefault();
   let searchInput = docuent.querySelector("#seach-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
 }
 
 let SearchFormElement = document.querySelector("#search-form");
