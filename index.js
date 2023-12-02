0b2957438d947884f57ecafb9b4545b7e468804c
Vania Loredo <vanialoredo.vl@gmail.com>
2023-12-02 09:37:31 -0500

    adding axios and api key and url + search city value

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+  let apikey = "4efbdb701a7t3129900523a73bf040o1";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
 function citySearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", citySearchSubmit);
