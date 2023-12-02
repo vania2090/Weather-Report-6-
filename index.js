2038d099f8fe3a33837d309fa6e1bdb1e938514c
Vania Loredo <vanialoredo.vl@gmail.com>
2023-12-02 00:39:47 -0500

    add elements to js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function citySearchSubmit(event) {
   event.preventDefault();
   let searchInput = docuent.querySelector("#seach-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
 }
-
 let SearchFormElement = document.querySelector("#search-form");
 SearchFormElement.addEventListener("submit", citySearchSubmit);
