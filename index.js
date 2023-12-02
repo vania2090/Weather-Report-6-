47240b02b8f72c2fd2f929e50f89861da624b42c
Vania Loredo <vanialoredo.vl@gmail.com>
2023-12-02 18:44:19 -0500

    adding time element and date

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ function currentWeather(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
+
+  timeElement.innerHTML = `${date.getHours()}:${date.getMinutes()}`;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed}miles/h`;
