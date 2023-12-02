67203d481edb07c84ed4b7ad74425841db32c462
Vania Loredo <vanialoredo.vl@gmail.com>
2023-12-02 18:32:39 -0500

    added wind

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ function currentWeather(response) {
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
 
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
-  humidityElement.innerHTML = `${response.data.temperature.humidity}`;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}miles/h`;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
