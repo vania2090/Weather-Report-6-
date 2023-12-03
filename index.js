2d05320fe67331eef8051aac60332552fe833299
Vania Loredo <vanialoredo.vl@gmail.com>
2023-12-02 19:03:46 -0500

    updating hrs

--- index.js
+++ index.js
@@ -30,14 +30,12 @@ function formatDate(date) {
     "Saturday",
   ];
   let day = days[date.getDay()];
+
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
 
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  return `${formattedDay} ${hours}:${minutes}`;
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
