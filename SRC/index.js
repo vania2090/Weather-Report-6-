4e7c0571d4cc047cbdec8b8a60f7ee777323b9aa
Vania Loredo <vanialoredo.vl@gmail.com>
2023-12-09 18:53:18 -0500

    added day and forecast Html to js

--- SRC/index.js
+++ SRC/index.js
@@ -51,18 +51,17 @@ function citySearchSubmit(event) {
   let searchInput = document.querySelector("#search-form-input");
   searchCity(searchInput.value);
 }
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", citySearchSubmit);
-
-searchCity("Sacramento");
 
 function displayForecast() {
-  let forecast = document.querySelector("#forecast");
-  let days = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thur", "Fri"];
+  let days = ["Sat", "Sun", "Mon", "Tues", "Wed"];
+  let forecastHtml = "";
+
   days.forEach(function (day) {
-    forecast.innerHTML = `
+    forecastHtml =
+      forecast.innerHtml +
+      `
       <div class="weather-forecast-day">
-        <div class="weather-forecast-date">Sat</div>
+        <div class="weather-forecast-date">${day}</div>
         <img
           src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAYxJREFUaN7tmMERgyAQRS2BEizBEizBEiyBEizBEizBEiyB679Zgh1sLpsMIRgRAZOZdeYfNBPY94FdoCKi6p9VCYAACIAACIAAvF5OPgAUgBHACoAsrfxdVQmfpAAAOgCbE7irDUD3cwAA+oPAXXW3AABoAczs5MKuqwDnfSOhigJwsG4gDc9titDA/x8cNbkAPhbmzvcUMiEgwQDslNvJwr9RRvWpAFpP4xOAOjMAfRuJIAArt3vTYQEAEw3Awa8e55WVkeiuUQgBmD2ZQxUM/NVvLIDPeVM4+CQA603OXwZ4uq13MlEpLVah0wDqUADNDdzp/p7Gs5WYflDTvwMQgP4OgM2ey1zRdcSulgCY0gDGKoQTL9CJ3+00vbAO24zdjcY6rzhg78LcOabOKQCGBAAh6bhnwM0poNNVABU5R23V3wI5qAN7/ZszR8rOc4IKFrexXIDvPe22ya5VDq5bngs2dhTbrNcqBwAmUQIYiwNk2EPp0gBNrp2pXO4KgAAIgAAIgAAIgAC86wECCuvGtH3EIQAAAABJRU5ErkJggg=="
           alt=""
@@ -75,5 +74,11 @@ function displayForecast() {
       </div>
     `;
   });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
 }
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", citySearchSubmit);
+
+searchCity("Sacramento");
 displayForecast();
