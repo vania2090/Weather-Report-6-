e6142576428545c9810d13d6ee276e39be6e24ac
Vania Loredo <vanialoredo.vl@gmail.com>
2023-12-09 20:09:57 -0500

    added icon and daily functions

--- SRC/index.js
+++ SRC/index.js
@@ -59,25 +59,26 @@ function getForecast(city) {
   axios.get(apiUrl).then(displayForecast);
 }
 function displayForecast(response) {
-  let days = ["Sat", "Sun", "Mon", "Tues", "Wed"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
+  response.data.daily.forEach(function (day) {
     forecastHtml =
       forecastHtml +
       `
-      <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${day}</div>
-        <img
-          src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAYxJREFUaN7tmMERgyAQRS2BEizBEizBEiyBEizBEizBEiyB679Zgh1sLpsMIRgRAZOZdeYfNBPY94FdoCKi6p9VCYAACIAACIAAvF5OPgAUgBHACoAsrfxdVQmfpAAAOgCbE7irDUD3cwAA+oPAXXW3AABoAczs5MKuqwDnfSOhigJwsG4gDc9titDA/x8cNbkAPhbmzvcUMiEgwQDslNvJwr9RRvWpAFpP4xOAOjMAfRuJIAArt3vTYQEAEw3Awa8e55WVkeiuUQgBmD2ZQxUM/NVvLIDPeVM4+CQA603OXwZ4uq13MlEpLVah0wDqUADNDdzp/p7Gs5WYflDTvwMQgP4OgM2ey1zRdcSulgCY0gDGKoQTL9CJ3+00vbAO24zdjcY6rzhg78LcOabOKQCGBAAh6bhnwM0poNNVABU5R23V3wI5qAN7/ZszR8rOc4IKFrexXIDvPe22ya5VDq5bngs2dhTbrNcqBwAmUQIYiwNk2EPp0gBNrp2pXO4KgAAIgAAIgAAIgAC86wECCuvGtH3EIQAAAABJRU5ErkJggg=="
-          alt=""
-          width="40"
-        />
-        <div class="weather-temps">
-          <span class="weather-temps-high">18</span>
-          <span class="weather-temps-low">12</span>
+        <div class="weather-forecast-day">
+          <div class="weather-forecast-date">Tues</div>
+
+            <img src="${day.condition.icon_url}"class="weather-forecast-icon" />
+
+          <div class="weather-temps">
+            <span class="weather-temps-high">
+              ${Math.round(day.temperature.maximum)}
+            </span>
+            <span class="weather-temps-low">
+              ${Math.round(day.temperature.minimum)}
+            </span>
+          </div>
         </div>
-      </div>
     `;
   });
   let forecastElement = document.querySelector("#forecast");
